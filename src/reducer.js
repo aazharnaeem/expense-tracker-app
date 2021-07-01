e9db5fff494c786a6149bd1f08c67950d1345d4b
ahmedhassan2k <ahmedhassan2k@gmail.com>
2021-07-02 02:37:15 +0500

    Added case for delete transaction.

--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,11 @@
 const transactionReducer = (state, action) => {
     switch (action.type) {
-        case 'ADD_TRANSACTION':
+        case 'ADD_TRANSACTION':{
             return [action.payload, ...state]
+        }
+        case 'DELETE_TRANSACTION':{
+            return [ ...state.filter(transaction => transaction.id !== action.payload)]
+        }
         default:
             return state;
     };
